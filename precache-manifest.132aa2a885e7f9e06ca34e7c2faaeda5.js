ffa881b39ea52694e8fbf82d35bdc3c33d8065ff
Tim Liu <hylswind@gmail.com>
2023-07-05 15:25:23 +0800

    Upload

---
+++ precache-manifest.132aa2a885e7f9e06ca34e7c2faaeda5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07e44a20898406f60643aaacd1f5c9b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae0e89c7949593cf03d2",
+    "url": "/static/css/3.d3a7edb5.chunk.css"
+  },
+  {
+    "revision": "607f451cb674b67fe028",
+    "url": "/static/css/4.9e685923.chunk.css"
+  },
+  {
+    "revision": "1960fe92201de16ebb41",
+    "url": "/static/css/5.ed59a352.chunk.css"
+  },
+  {
+    "revision": "ba4a36fc4a02204568f5",
+    "url": "/static/css/main.1232e35b.chunk.css"
+  },
+  {
+    "revision": "40c3d213a558f63a9e09",
+    "url": "/static/js/2.3af65602.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3af65602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae0e89c7949593cf03d2",
+    "url": "/static/js/3.de175701.chunk.js"
+  },
+  {
+    "revision": "607f451cb674b67fe028",
+    "url": "/static/js/4.914a2be7.chunk.js"
+  },
+  {
+    "revision": "1960fe92201de16ebb41",
+    "url": "/static/js/5.a352ded1.chunk.js"
+  },
+  {
+    "revision": "ba4a36fc4a02204568f5",
+    "url": "/static/js/main.04acdca1.chunk.js"
+  },
+  {
+    "revision": "a37284f249b0edd7f808",
+    "url": "/static/js/runtime-main.967fb6a8.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
